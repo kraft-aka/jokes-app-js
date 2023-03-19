b46ed2cbf4ce1a33358c48380015be3dc1fc1b57
kubat <kubat.aiupov@gmail.com>
2023-03-19 20:31:26 +0100

    init gitignore

---
+++ app.js
@@ -0,0 +1,8 @@
+const jokeBtn = document.querySelector('.joke-btn');
+const MemeBtn = document.querySelector('.meme-btn');
+
+const URL = 'https://api.humorapi.com/jokes/random'
+
+const getData = () => {
+
+}
